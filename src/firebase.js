3c7c5e8db84f6de4a24472097b32f2def632d203
bwanking <84254991+bwanking@users.noreply.github.com>
2026-02-04 21:27:51 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,52 +1,14 @@
 import { initializeApp } from "firebase/app";
-import {
-  GoogleAuthProvider,
-  GithubAuthProvider,
-  getAuth,
-  signInWithPopup,
-  signOut,
-} from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCgq9_xbC2zMLV_gIrGlBnpryRN_toiHW4",
-  authDomain: "login-signup-auth-ks.firebaseapp.com",
-  projectId: "login-signup-auth-ks",
-  storageBucket: "login-signup-auth-ks.appspot.com",
-  messagingSenderId: "153852426357",
-  appId: "1:153852426357:web:0dace21431a9f096a2203c",
+  apiKey: "AIzaSyDFpRE1lMCLox32brF5UB5vgX0oTzTftHM",
+  authDomain: "asa-dba.firebaseapp.com",
+  projectId: "asa-dba",
+  storageBucket: "asa-dba.firebasestorage.app",
+  messagingSenderId: "987392198785",
+  appId: "1:987392198785:web:056ef33fbb1f0516a62580"
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-const googleProvider = new GoogleAuthProvider();
-const signInWithGoogle = async () => {
-  try {
-    const res = await signInWithPopup(auth, googleProvider);
-    const user = res.user;
-    console.log(user);
-  } catch (err) {
-    console.error(err);
-    alert(err.message);
-  }
-};
-
-const githubProvider = new GithubAuthProvider();
-const signInWithGithub = async () => {
-  try {
-    const res = await signInWithPopup(auth, githubProvider);
-    const user = res.user;
-    console.log(user);
-  } catch (err) {
-    console.error(err);
-    alert(err.message);
-  }
-};
-
-const logout = () => {
-  signOut(auth);
-};
-
-export { app, auth, db, signInWithGoogle, signInWithGithub, logout };
+export const db = getFirestore(app);
